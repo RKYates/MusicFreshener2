c0d834c835655edfbde452d522e969a10906edff
ryates <robert.k.yates@gmail.com>
2024-10-29 20:35:37 -0400

    Initial layout for bottomsheet to add new entry

--- app/src/main/java/com/example/musicfreshener2/MainActivity.kt
+++ app/src/main/java/com/example/musicfreshener2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.musicfreshener2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -10,34 +11,156 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.musicfreshener2.ui.theme.MusicFreshener2Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MusicFreshener2Theme {
-                Scaffold(
-                    bottomBar = { BottomBar() },
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    MusicList(innerPadding)
+                ShowScreen()
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun ShowScreen() {
+
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showAddBottomSheet by remember { mutableStateOf(false) }
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = App.appContext.getString(R.string.menu_title))
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { showAddBottomSheet = true },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = App.appContext.getString(R.string.add_entry))
+                    }
+                }
+            )
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+
+        MusicList(innerPadding)
+
+        if (showAddBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showAddBottomSheet = false
+                },
+                sheetState = sheetState,
+
+            ) {
+                // Sheet content
+                var artist by remember { mutableStateOf("") }
+                var album by remember { mutableStateOf("") }
+                var rating by remember { mutableStateOf("") }
+                var date by remember { mutableStateOf("") }
+                var genre by remember { mutableStateOf("") }
+
+                Column (
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    TextField(
+                        value = artist,
+                        onValueChange = { artist = it },
+                        label = { Text(stringResource(R.string.artist)) },
+                        modifier = Modifier.padding(8.dp),
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = androidx.compose.ui.text.input.ImeAction.Next)
+                    )
+                    TextField(
+                        value = album,
+                        onValueChange = { album = it },
+                        label = { Text(stringResource(R.string.album)) },
+                        modifier = Modifier.padding(8.dp),
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = androidx.compose.ui.text.input.ImeAction.Next)
+                    )
+                    TextField(
+                        value = genre,
+                        onValueChange = { genre = it },
+                        label = { Text(stringResource(R.string.genre)) },
+                        modifier = Modifier.padding(8.dp),
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = androidx.compose.ui.text.input.ImeAction.Next)
+                    )
+                    TextField( // TODO make app scroll to this when focus is set to it
+                        value = date,
+                        onValueChange = { date = it },
+                        label = { Text(stringResource(R.string.date)) },
+                        modifier = Modifier.padding(8.dp),
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = androidx.compose.ui.text.input.ImeAction.Next)
+                    )
+                    TextField( // TODO make app scroll to this when focus is set to it
+                        value = rating,
+                        onValueChange = { rating = it },
+                        label = { Text(stringResource(R.string.rating)) },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        singleLine = true,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+                Button(onClick = {
+                    addEntry(MusicEntry(artist.trim(), album.trim(), rating.toInt(), date, genre.trim()))
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            showAddBottomSheet = false
+                        }
+                    }
+                },
+                    modifier = Modifier.padding(bottom = 100.dp) // TODO make this go higher
+                ) {
+                    Text(stringResource(R.string.add_album))
                 }
             }
         }
@@ -110,6 +233,11 @@ fun MusicRow(entry: MusicEntry) {
             Text(text = entry.genre)
         }
     }
+}
+
+fun addEntry(entry: MusicEntry) {
+    //TODO: Add entry to list
+    Log.d("MusicFreshener2", "addEntry: $entry")
 }
 
 @Preview(showBackground = true)
